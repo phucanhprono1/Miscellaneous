f2eb7612fbb80d3f0b30acafbad1db6929f1d7ba
phucanhprono1 <90205324+phucanhprono1@users.noreply.github.com>
2024-03-08 09:36:41 +0700

    new skeleton and draft code

---
+++ Miscellaneous-MQ/src/test/java/com/mscs/miscellaneousmq/MiscellaneousMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mscs.miscellaneousmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiscellaneousMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
