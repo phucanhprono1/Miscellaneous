2f51a5649cadf830a0b8a2b78bd1a60da3e409b1
phucanhprono1 <90205324+phucanhprono1@users.noreply.github.com>
2024-03-08 09:36:41 +0700

    new skeleton and draft code

---
+++ Miscellaneous-MQ/src/main/java/com/mscs/miscellaneousmq/MiscellaneousMqApplication.java
@@ -0,0 +1,13 @@
+package com.mscs.miscellaneousmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiscellaneousMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiscellaneousMqApplication.class, args);
+    }
+
+}
